a4d9469a01427aa51adb9e7bd626ef85aff786ff
foxz1n <148496806+foxzinnx@users.noreply.github.com>
2025-01-24 15:39:33 -0300

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,7 +1,20 @@
-function calcular(){
+function validar(){
     var num1 = document.getElementById('first').value;
     var num2 = document.getElementById('second').value;
     var operator = document.getElementById('operador').value;
+
+    if(num1 != '' && num2 != ''){
+        var resultado = this.calcular(num1, num2, operator);
+        document.getElementById("result-num").textContent = resultado;
+        document.querySelector(".div-result").style.display = "flex";
+        document.querySelector(".null").style.display = "none";
+    }else{
+        document.querySelector(".div-result").style.display = "none";
+        document.querySelector(".null").style.display = "flex";
+    }
+}
+
+function calcular(num1, num2, operator){
 
     var num1 = parseFloat(num1);
     var num2 = parseFloat(num2);
@@ -22,7 +35,5 @@ function calcular(){
         var resultado = num1 * num2;
     }
 
-    document.getElementById("result-num").textContent = resultado;
-    document.querySelector(".div-result").style.display = "flex";
-    console.log(resultado)
+    return resultado;
 }
